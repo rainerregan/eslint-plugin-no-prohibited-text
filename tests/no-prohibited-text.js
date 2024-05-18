f6766a593294ab1a9277c565e5df4dbe5b3df2c6
Rainer Regan (Exacode) <rainercoc@gmail.com>
2024-05-18 22:00:46 +0700

    refactor: Update code to separate the text list

--- tests/no-prohibited-text.js
+++ tests/no-prohibited-text.js
@@ -1,23 +1,29 @@
 const { RuleTester } = require('eslint');
 const rule = require('../lib/rules/no-prohibited-text');
+const defaultProhibitedNames = require('../lib/default-values');
 
 const ruleTester = new RuleTester();
 
 ruleTester.run('no-prohibited-text', rule, {
   valid: [
-    { code: 'let allowedVariable;', options: [['prohibitedVariable']] },
-    { code: 'const safeName = 1;', options: [['bannedName']] }
+    { code: 'let allowedVariable;' }, // No prohibited variable names provided
+    { code: 'const safeName = 1;' },  // No prohibited variable names provided
+    { code: 'let customVariable;', options: [['prohibitedVariable']] }, // Custom option
   ],
   invalid: [
+    ...defaultProhibitedNames.map(name => ({
+      code: `let ${name};`,
+      errors: [{ messageId: 'prohibitedText', data: { name } }]
+    })),
     {
       code: 'let prohibitedVariable;',
       options: [['prohibitedVariable']],
-      errors: [{ messageId: 'prohibitedText', data: { name: 'prohibitedVariable' } }]
+      errors: [{ messageId: 'prohibitedText', data: { name: 'prohibitedVariable' } }] // Custom option
     },
     {
       code: 'const bannedName = 1;',
       options: [['bannedName']],
-      errors: [{ messageId: 'prohibitedText', data: { name: 'bannedName' } }]
+      errors: [{ messageId: 'prohibitedText', data: { name: 'bannedName' } }] // Custom option
     }
   ]
 });
