375b932c39320930ea856355bff5d797688280ae
Rainer Regan (Exacode) <rainercoc@gmail.com>
2024-05-18 22:02:43 +0700

    refactor: Update test case

---
+++ tests/sample.js
@@ -0,0 +1,4 @@
+let foo; // This should cause an ESLint error
+let bar; // This should cause an ESLint error
+const baz = 1; // This should cause an ESLint error
+let allowedVariable; // This should not cause an ESLint error
